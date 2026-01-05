1f28bf0373c587b2089d5915dfc817684053ba59
Prem Raval <ravalprem17@gmail.com>
2026-01-05 21:51:14 +0530

    arr added in for loop

---
+++ unit 2/printarr.py
@@ -0,0 +1,17 @@
+print('\n Print the list with for loop\n')
+a = [1,5,'Hello',1+2j,'another string']
+for i in a:
+    print(i)
+
+print('\n Print tuple with for loop\n')
+b = (1,5,'string1',6.343, 'another string',1+2j,5.5)
+for i in b:
+    print(b)
+
+print('\n Print set with for loop\n')
+c = {1,5,'Hello',1+2j,'another string',50.50,2000000}
+
+for i in c:
+    print(i)
+
+print('\n')
